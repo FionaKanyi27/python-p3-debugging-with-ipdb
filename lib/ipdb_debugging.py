f490241d8c7f50cef96c25fa7ede3a3302167634
Fiona <fionakanyi0@gmail.com>
2025-04-24 21:27:51 +0300

    lib

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -5,3 +5,17 @@ import ipdb
 def plus_two(num):
     num = num + 2
     return num
+
+def tracing_the_function():
+    import ipdb
+
+    inside_the_function = "We're inside the function"
+    print(inside_the_function)
+    print("We're about to stop because of ipdb!")
+    ipdb.set_trace()
+    this_variable_hasnt_been_interpreted_yet = \
+        "The program froze before it could read me!"
+    print(this_variable_hasnt_been_interpreted_yet)
+
+if __name__ == "__main__":
+    tracing_the_function()
